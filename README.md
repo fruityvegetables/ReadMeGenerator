a2e2b668d0ece4d622a12f3ed6fcb6bcb030cbf6
fruityvegetables <gg.troyer@gmail.com>
2020-04-10 19:52:32 -0500

    completed the generator, missing badges

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
-title
-badge
-desc
-install
-usage
-license
-contrib
-testings
-question
+# tits bads
+## Description
+asdfasd
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Credits](#contributing)
+* [Testing](#tests)
+* [Questions](#questions)
+## Installation
+asdf
+## Usage
+fdsa
+## License
+asdf
+## Contributing
+asdf
+## Tests
+fasd
+## Questions
+![avatarImage](https://avatars1.githubusercontent.com/u/32282285?v=4) | [Link to my GitHub](https://github.com/fruityvegetables)
--- index.js
+++ index.js
@@ -18,98 +18,109 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
+ inquirer
+    .prompt([{
+      message: "Enter the title of your project: ",
+      name: "title"
+    },
+    {
+      message: "Enter the badge for your project: ",
+      name: "badge"
+    },
+    {
+      message: "Give a description of your project: ",
+      name: "description"
+    },
+    {
+      message: "Put something in the installation section: ",
+      name: "install"
+    },
+    {
+      message: "Put something in the usage section: ",
+      name: "usage"
+    },
+    {
+      message: "What type of license will you be using?: ",
+      name: "license"
+    },
+    {
+      message: "Talk about contributing here: ",
+      name: "contrib"
+    },
+    {
+      message: "Describe your testing methods: ",
+      name: "tests"
+    }])
+  .then(function (data) {
+    //console.log(title, badge, description, install, usage, license, contrib, tests, questions);
+    const { title, badge, description, install, usage, license, contrib, tests, questions } = data;
+    // console.log(title);
+    // console.log(contrib);
+    const template = `# ${title} ${badge}
+## Description
+${description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Credits](#contributing)
+* [Testing](#tests)
+* [Questions](#questions)
+## Installation
+${install}
+## Usage
+${usage}
+## License
+${license}
+## Contributing
+${contrib}
+## Tests
+${tests}
+## Questions
+`;
+    return template
+  })
+  .then(function (html) {
+    writeFileAsync("README.md", html)
+    console.log("You created a README!");
+  })
+  .then(function(){
+    return inquirer
+    .prompt({
+      message: "Enter your GitHub username:",
+      name: "username"
+    })
+
   })
   .then(function ({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`;
 
+    return axios
+      .get(queryUrl)
+      .then(function (res) {
+        const avatar = res.data.avatar_url;
+        const username = res.data.login;
+        //console.log(res.data);
+        return [avatar, username];
+        
+        // const email = res.data.email;      
+        //  console.log(res);
+        //   const repoNames = res.data.map(function(repo) {
+        //     return repo.name;
+      })
 
-    axios.get(queryUrl).then(function (res) {
-      const avatar = res.data.avatar_url;
-      const email = res.data.email;
-      //  console.log(res);
-      //   const repoNames = res.data.map(function(repo) {
-      //     return repo.name;
-    }).then(
-      inquirer
-        .prompt([{
-          message: "Enter the title of your project: ",
-          name: "title"
-        },
-        {
-          message: "Enter the badge for your project: ",
-          name: "badge"
-        },
-        {
-          message: "Give a description of your project: ",
-          name: "description"
-        },
-        {
-          message: "Put something in the installation section: ",
-          name: "install"
-        },
-        {
-          message: "Put something in the usage section: ",
-          name: "usage"
-        },
-        {
-          message: "What type of license will you be using?: ",
-          name: "license"
-        },
-        {
-          message: "Talk about contributing here: ",
-          name: "contrib"
-        },
-        {
-          message: "Describe your testing methods: ",
-          name: "tests"
-        },
-        {
-          message: "Add some questions if appropriate: ",
-          name: "questions"
-        }])
-        .then(function ({ title, badge, description, install, usage, license, contrib, tests, questions }) {
-          //console.log(title, badge, description, install, usage, license, contrib, tests, questions);
-          let array = [title, badge, description, install, usage, license, contrib, tests, questions];
-          const content = array.join("\n");
-          fs.writeFile("README.md", content, function (err) {
-            if (err) {
-              throw err;
-            } else {
-              return `
-              # ${title} ${badge}
-              ## Description
-              ${description}
-              ## Table of Contents
-              * [Installation](#install)
-              * [Usage](#usage)
-              * [License](#license)
-              * [Contribution](#contrib)
-              * [Testing](#tests)
-              * [Questions](#questions)
-              ## Installation
-              ${install}
-                ## Usage
-              ${usage}
-              ## License
-              ${license}
-              ## Contributing
-              ${contrib}
-              ## Tests
-              ${tests}
-              ## Questions
-              ${questions}
-              `;
-            }
-            console.log("done");
-          })
-        })
 
-    )
+  })
+  .then(function (array) {
+    const ava = `![avatarImage](${array[0]}) | [Link to my GitHub](https://github.com/${array[1]})`;
+    appendFileAsync("README.md", ava)
+    console.log("You appended your GitHub image to this README!");
+  });
+
+
+
     //   inquirer
     //   .prompt({
     //     type: "list",
@@ -127,4 +138,3 @@ inquirer
     //     //console.log(`Saved ${repoNames.length} repos`);
     //   });
     // });
-  });
